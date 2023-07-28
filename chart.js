e1e92b42ad22f74d5d3bc523ac9c970b5e0ac584
Alireza Balaei <alirezabalayi7@gmail.com>
2023-07-28 14:30:48 +0200

    add some assistant for data management and some initializing xaxis for chart

--- chart.js
+++ chart.js
@@ -1,18 +1,51 @@
 // Global variables
 let data = []
 let tempData = []
-let width = 1000
-let height = 600
+let chartBox = {}
+let col_rel = {}
+let binding = {"labels": 0,"values": 1,"groups": 2}
+let width = 0
+let height = 0
 
 // Draw chart with given data
 const draw = ({data}) => {
-  console.log(data, width, height)
+  removeSvg()
+  
+  // Declare main svg to draw our chart inside it
+  console.log(width, height)
+  var mainSvg = d3
+    .select("#chart-wrapper")
+    .append("svg")
+    .attr("class", "mainSvg")
+    .attr("width", width)
+    .attr("height", height)
+
+  var dataValues = data.map(e=>e[col_rel["values"]])
+  var minData = d3.min(data.map(e=>e[col_rel["values"]]))
+  var maxData = d3.max(data.map(e=>e[col_rel["values"]]))
+  console.log(minData, maxData, dataValues)
+  var xScale = d3.scaleLinear().domain([minData,maxData]).range([0,width])
+  var xAxix = mainSvg
+    .append("g")
+    .attr("class", "xAxis")
+    .attr("id", "xAxis")
+    .attr("transform", `translate(${0}, ${height})`)
+    .call(d3.axisBottom(xScale))
+
 }
 
+const removeSvg = () => {
+  d3.select("#chart-wrapper").selectAll("*").remove()
+}
 // Resize
-const resizeHandler = () => {
-  width = window.innerWidth
-  height = window.innerHeight
+const resizeHandler = (event) => {
+  if(event){
+    width = event.currentTarget.innerWidth
+    height = event.currentTarget.innerHeight
+  } else { // First time
+    width = window.innerWidth
+    height = window.innerHeight
+  }
   draw({data: tempData})
 }
 
@@ -23,6 +56,11 @@ const transformData = (newData) => {
  
   // Declare tempData to work with
   tempData = structuredClone(newData)
+
+  // Create col_rel to refer the data easier
+  Object.keys(binding).forEach(
+    e => col_rel[e] = tempData.columns[binding[e]]
+  )
 
   // Create a resizeHandler event with d3
   d3.select(window).on('resize.updatesvg', resizeHandler)
