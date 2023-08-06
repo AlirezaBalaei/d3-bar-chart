6f0fd22caf2bf8369f067cdd10566d734b2bce79
Alireza Balaei <alirezabalayi7@gmail.com>
2023-08-06 20:05:01 +0200

    added xAxis + resolution scales

--- chart.js
+++ chart.js
@@ -7,30 +7,52 @@ let binding = {"labels": 0,"values": 1,"groups": 2}
 let width = 0
 let height = 0
 
+// Configs
+config = {}
+config.xAxis = {}
+config.xAxis.label = {}
+config.xAxis.label.color = "#03396c"
+
 // Draw chart with given data
 const draw = ({data}) => {
   removeSvg()
   
   // Declare main svg to draw our chart inside it
-  console.log(width, height)
   var mainSvg = d3
     .select("#chart-wrapper")
     .append("svg")
     .attr("class", "mainSvg")
     .attr("width", width)
     .attr("height", height)
+  
+  // Resolution Scales
+  var resolutionScale = d3.scaleLinear().domain([0,1000*600]).range([0,1]).clamp(true)(width*height)
+  var resolutionScaleX = d3.scaleLinear().domain([0,600]).range([0,1]).clamp(true)(width)
+  var resolutionScaleY = d3.scaleLinear().domain([0,1000]).range([0,1]).clamp(true)(height)
 
   var dataValues = data.map(e=>e[col_rel["values"]])
   var minData = d3.min(data.map(e=>e[col_rel["values"]]))
   var maxData = d3.max(data.map(e=>e[col_rel["values"]]))
-  console.log(minData, maxData, dataValues)
-  var xScale = d3.scaleLinear().domain([minData,maxData]).range([0,width])
-  var xAxix = mainSvg
+  
+  // X Axis
+  var XAxisWidth = width * 0.90
+  XAxisMargin = {}
+  XAxisMargin.left = (width - XAxisWidth) / 2
+  XAxisMargin.bottom = 20 * resolutionScaleY
+  var xScale = d3.scaleLinear().domain([parseFloat(minData),parseFloat(maxData)]).range([0,width * .9])
+  var x_axis = d3.axisBottom().scale(xScale).tickSize(20 * resolutionScaleY)
+  var xAxis = mainSvg
     .append("g")
     .attr("class", "xAxis")
     .attr("id", "xAxis")
-    .attr("transform", `translate(${0}, ${height})`)
-    .call(d3.axisBottom(xScale))
+    .call(x_axis)
+    .attr("transform", function(){
+      return `translate(${XAxisMargin.left}, ${height - this.getBBox().height - XAxisMargin.bottom})`
+    })
+  var xAxisLabel = xAxis
+    .selectAll("text")
+    .attr("fill", config.xAxis.label.color)
+    .attr("font-size", 20 * resolutionScale)
 
 }
 
@@ -49,7 +71,6 @@ const resizeHandler = (event) => {
   draw({data: tempData})
 }
 
-// Draw the barchart
 const transformData = (newData) => {
   // Assign global data variable
   data = newData
