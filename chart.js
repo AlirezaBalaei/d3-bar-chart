f0ca44a3a28fceebb431c87f33c4fc819fb6c708
Alireza Balaei <alirezabalayi7@gmail.com>
2023-08-13 21:06:13 +0330

    added yAxis

--- chart.js
+++ chart.js
@@ -35,32 +35,68 @@ const draw = ({data}) => {
   var minData = d3.min(data.map(e=>e[col_rel["values"]]))
   var maxData = d3.max(data.map(e=>e[col_rel["values"]]))
   
+  // Y Axis
+  var YAxisHeight = height * 0.90
+  var YAxisWidth = 0
+  YAxisMargin = {}
+  YAxisMargin.top = (height - YAxisHeight) / 2
+  YAxisMargin.left = 20 * resolutionScaleX
+  var dataLabels = data.map(e=>e[col_rel["labels"]])
+  var yScale = d3.scaleBand().domain(dataLabels).range([0, YAxisHeight])
+  var y_axis = d3.axisLeft().scale(yScale).tickSizeInner(0).tickSizeOuter(0)
+  var yAxis = mainSvg
+    .append("g")
+    .attr("id", "yAxis")
+    .call(y_axis)
+  var yAxisLabel = yAxis
+    .selectAll("text")
+    .attr("fill", config.xAxis.label.color)
+    .attr("font-size", 20 * resolutionScale)
+    .attr("font-family", "Plus Jakarta Sans")
+  var yAxisLine = yAxis
+    .selectAll("line,path")
+    .attr("stroke", config.xAxis.tick.color)
+    .attr("stroke-width", 0.3)
+  yAxis
+    .attr("transform", function(){
+      yAxisWidth = this.getBBox().width + YAxisMargin.left
+      return `translate(${this.getBBox().width + YAxisMargin.left},${YAxisMargin.top})`
+    })
+
   // X Axis
-  var XAxisWidth = width * 0.90
-  XAxisMargin = {}
-  XAxisMargin.left = (width - XAxisWidth) / 2
-  XAxisMargin.bottom = 20 * resolutionScaleY
-  var xScale = d3.scaleLinear().domain([parseFloat(minData),parseFloat(maxData)]).range([0,width * .9])
-  var x_axis = d3.axisBottom().scale(xScale).tickSize(20 * resolutionScaleY)
+  var XAxisWidth = width * 0.95
+  var xScale = d3.scaleLinear().domain([parseFloat(minData),parseFloat(maxData)]).range([0,XAxisWidth-yAxisWidth])
+  var x_axis = d3.axisBottom().scale(xScale).tickSizeInner(0).tickSizeOuter(0)
   var xAxis = mainSvg
     .append("g")
     .attr("class", "xAxis")
     .attr("id", "xAxis")
     .call(x_axis)
     .attr("transform", function(){
-      return `translate(${XAxisMargin.left}, ${height - this.getBBox().height - XAxisMargin.bottom})`
+      return `translate(${yAxisWidth}, ${YAxisHeight+YAxisMargin.top})`
     })
   var xAxisLabel = xAxis
     .selectAll("text")
     .attr("fill", config.xAxis.label.color)
     .attr("font-size", 20 * resolutionScale)
     .attr("font-family", "Plus Jakarta Sans")
+  // Move first and last label inside
+  xAxisLabel
+    .attr("transform", function(d,i){
+      var labeWidth = this.getBBox().width
+      var tx = 0
+      if(i == xAxisLabel.size() - 1)
+        tx = - labeWidth/2
+      if(i == 0)
+        tx = labeWidth / 2
+      return `translate(${tx},${0})`
+    })
+
     
   var xAxisLine = xAxis
     .selectAll("line,path")
     .attr("stroke", config.xAxis.tick.color)
     .attr("stroke-width", 0.3)
-
 }
 
 const removeSvg = () => {
